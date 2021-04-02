f4fc38840544414e6dac3ee57d48d9e5547a2ed1
Дмитрий Валерьевич Буйлов <buylov@MK7.corp>
2021-04-02 17:01:02 +0300

    fix

--- advanced/frontend/views/application/test-form.php
+++ advanced/frontend/views/application/test-form.php
@@ -1,9 +1,10 @@
 <div class="">
 
     <form id="test-form" action="/application/feedback" method="post">
-        <input type="text" name="status" value="получено / отказано" />
-        <input type="text" name="id" />
-        <input type="text" name="comment" />
+<!--        <input type="text" name="status" value="получено / отказано" />-->
+<!--        <input type="text" name="id" />-->
+<!--        <input type="text" name="comment" />-->
+        <input type="text" name="data" />
         <button type="submit" name="button">Submit</button>
     </form>
 
