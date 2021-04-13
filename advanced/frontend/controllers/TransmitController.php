031d28a9873666310acbfe05baf3ae1c5a9913f1
Дмитрий Валерьевич Буйлов <buylov@MK7.corp>
2021-04-13 15:47:24 +0300

    fix

--- advanced/frontend/controllers/TransmitController.php
+++ advanced/frontend/controllers/TransmitController.php
@@ -57,7 +57,7 @@ class TransmitController extends Controller
             $model->updated = time();
             $model->status = 1;
             $model->save();
-            $response_body['u' . $model->uid] = 'u' . $model->uid;
+            $response_body[] = ['uid' => $model->uid];
         }
 //        return implode("|", $response_body);
         return json_encode($response_body, JSON_UNESCAPED_UNICODE);
