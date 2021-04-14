a7472f5093ad9dadfbd8631652189aaa394deb55
Дмитрий Валерьевич Буйлов <buylov@MK7.corp>
2021-04-14 09:22:53 +0300

    fix

--- advanced/frontend/controllers/TransmitController.php
+++ advanced/frontend/controllers/TransmitController.php
@@ -62,33 +62,33 @@ class TransmitController extends Controller
 //        return implode("|", $response_body);
 //        return json_encode($response_body, JSON_UNESCAPED_UNICODE);
         return '[
-                    {
-                        "programs":{"appl18":{"program3":"Сестринское дело очно-заочная, после 11 кл., договор"}},		
-                        "program_base":"2",		program_type":"очно-заочно",
-                        "program_financing":"договор",
-                        "user_id":69,
-                        "user_email":"builov@inbox.ru",
-                        "user_lastname":"Буйлов",
-                        "user_firstname":"Дмитрий",
-                        "user_patronim":"",
-                        "user_birthdate":"1970-09-24",
-                        "user_region":4,"user_snils":"",
-                        "user_gender":"Мужской",
-                        "user_education_level":5,
-                        "user_institution":"ПТУ №1",
-                        "user_graduate_year":2020,
-                        "user_certificate_series":"",
-                        "user_certificate_number":"1234567",
-                        "user_passport_series":"",
-                        "user_passport_number":"234234",
-                        "user_passport_issued":"УВД",
-                        "user_passport_code":"",
-                        "user_passport_date":"2020-11-11",
-                        "user_address_passport":"121108, г. Москва, ул. Пивченкова, дом 12, квартира 31",
-                        "user_address_current":"121108, г. Москва, ул. Пивченкова, дом 12, квартира 31",
-                        "user_phone":"+7 (123) 123-12-31",
-                        "user_files":["https:\/\/lks.medcollege7.ru\/uploads\/user69_passport.pdf","https:\/\/lks.medcollege7.ru\/uploads\/user69_education.pdf"]
-                    }
-                ]';
+{
+"programs":{"appl18":{"program3":"Сестринское дело очно-заочная, после 11 кл., договор"}},
+"program_base":"2",program_type":"очно-заочно",
+"program_financing":"договор",
+"user_id":69,
+"user_email":"builov@inbox.ru",
+"user_lastname":"Буйлов",
+"user_firstname":"Дмитрий",
+"user_patronim":"",
+"user_birthdate":"1970-09-24",
+"user_region":4,"user_snils":"",
+"user_gender":"Мужской",
+"user_education_level":5,
+"user_institution":"ПТУ №1",
+"user_graduate_year":2020,
+"user_certificate_series":"",
+"user_certificate_number":"1234567",
+"user_passport_series":"",
+"user_passport_number":"234234",
+"user_passport_issued":"УВД",
+"user_passport_code":"",
+"user_passport_date":"2020-11-11",
+"user_address_passport":"121108, г. Москва, ул. Пивченкова, дом 12, квартира 31",
+"user_address_current":"121108, г. Москва, ул. Пивченкова, дом 12, квартира 31",
+"user_phone":"+7 (123) 123-12-31",
+"user_files":["https:\/\/lks.medcollege7.ru\/uploads\/user69_passport.pdf","https:\/\/lks.medcollege7.ru\/uploads\/user69_education.pdf"]
+}
+]';
     }
 }
